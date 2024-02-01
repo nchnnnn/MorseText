4641957f96670df23cdb46a743e6b6020b1e994d
nchnnnn <92495274+nchnnnn@users.noreply.github.com>
2024-02-02 00:57:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var morseArea = $("#morse");
+var textArea = $("#text");
+
+const morse = {
+    "A": "·-",
+    "B": "-···",
+    "C": "-·-·",
+    "D": "-··",
+    "E": "·",
+    "F": "··-·",
+    "G": "--·",
+    "H": "····",
+    "I": "··",
+    "J": "·---",
+    "K": "-·-",
+    "L": "·-··",
+    "M": "--",
+    "N": "-·",
+    "O": "---",
+    "P": "·--·",
+    "Q": "--·-",
+    "R": "·-·",
+    "S": "···",
+    "T": "-",
+    "U": "··-",
+    "V": "···-",
+    "W": "·--",
+    "X": "-··-",
+    "Y": "-·--",
+    "Z": "--··",
+    ".": "·-·-·-",
+    ",": "--··--",
+    "'": "·----·",
+    "!": "-·-·--",
+    " ": "   "
+};
+
+const text = Object.fromEntries(
+    Object.entries(morse).map(([key, value]) => [value, key])
+  );
+
+function $(s) {
+    return document.querySelector(s);
+}
+
+function TxtToMorse(txt) {
+    if (textArea.value == ""){
+        morseArea.value = "";
+    };
+
+    const texts = textArea.value.toUpperCase();
+    const morseCode = texts.split("").map(char => morse[char] || " ");
+    const formattedMorse = morseCode.join(" "); // Add spaces for readability
+    morseArea.value = formattedMorse;
+}
+
+function MorseToTxt(morseArea) {
+    const morseCode = morseArea.value.trim().toUpperCase(); 
+    const words = morseCode.split("   "); // Split into words based on triple spaces
+    const output = words.map(word => word.split(" ").map(char => text[char] || "").join("")).join(" ");
+    textArea.value = output
+  }
